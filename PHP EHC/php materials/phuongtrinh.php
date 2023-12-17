fbd00811f633fc50c6327eccad6f51a10d7cbf24
TMQ <86949469+TMQrX@users.noreply.github.com>
2023-12-17 13:26:07 +0700

    Update phuongtrinh.php

--- PHP EHC/php materials/phuongtrinh.php
+++ PHP EHC/php materials/phuongtrinh.php
@@ -1,22 +1,27 @@
+<!DOCTYPE html>
 <html>
 <body>
-<form action="" method="POST">
-<br>phuong trinh bac nhat<br>
-<input type=text name=a>x +
-b <input type=text name=b> = 0
-<br><br><input type=submit value="Calculate">
-</form>
+    <h2>Phương trình bậc nhất</h2>
+    <form action="" method="POST">
+        <input type="text" name="a">x +
+        <input type="text" name="b"> = 0
+        <br><br>
+        <input type="submit" value="Calculate">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $a = isset($_POST["a"]) ? (float)$_POST["a"] : 0;
+        $b = isset($_POST["b"]) ? (float)$_POST["b"] : 0;
+
+        if ($a != 0) {
+            $x = (-$b / $a);
+            echo "<p>Nghiem cua pt: x = $x</p>";
+        } elseif ($b == 0) {
+            echo "<p>Phuong trinh vo so nghiem</p>";
+        } else {
+            echo "<p>Phuong trinh vo nghiem</p>";
+        }
+    }
+    ?>
 </body>
-<?php
-$a = $_POST["a"];
-$b = $_POST["b"];
-if ($a != 0) {
-    $x = (-$b / ($a));
-    echo "nghiem cua phuong trinh: x = $x";
-} elseif ($b == 0) {
-    echo "phuong trinh vo so nghiem";
-} else {
-    echo "phuong trinh vo nghiem";
-}
-?>
 </html>
